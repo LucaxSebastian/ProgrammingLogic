f685252e3a33361ba51b936f29b54e19d8fba6d6
Lucas Sebastian <lucassebastian167@gmail.com>
2022-05-27 22:43:14 -0300

    Main method added

---
+++ Sequential Structures (UDEMY)/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace LogicaProgramacao.EstruturaSequencial
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ListaDeExerciciosEstruturasSequenciais.ExibirListaDeExercicios();
+            ListaDeExerciciosEstruturasSequenciais.ExecutarExercicio();
+        }
+    }
+}
+
