d2fb8ad0cd35bc75977e7019c4c0d3813a960a18
Lucas Sebastian <lucassebastian167@gmail.com>
2022-05-29 17:36:34 -0300

    Adding the ListaExerciciosCondicionais.cs folder

---
+++ Conditional-Structures-UDEMY/ListaExerciciosCondicionais.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace LogicaProgramacao.EstruturaCondicional
+{
+    public static class ListaDeExerciciosCondicionais
+    {
+        public static void MenuDeExercicios()
+        {
+            Console.WriteLine($"--- Exercicios sobre Estruturas Condicionais --- \n");
+
+            Console.WriteLine("5) SAIR");
+
+        }
+
+        public static void ExecutarExercicio()
+        {
+            Console.Write("\nEscolha o número do exercício: ");
+            int.TryParse(Console.ReadLine(), out int opcao);
+            Console.WriteLine();
+
+            switch(opcao)
+            {
+                case 1:  break;
+
+
+
+                case 10: 
+                    Console.WriteLine("Saindo... Obrigado!");
+                    System.Environment.Exit(0);
+                break;
+
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente\n"); 
+                    MenuDeExercicios();
+                break;
+            }
+        }
+    }
+}
