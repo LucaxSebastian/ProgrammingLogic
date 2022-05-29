9a885cecffe3f723616fb7e473d1dbb3271cfbfc
Lucas Sebastian <lucassebastian167@gmail.com>
2022-05-29 20:37:20 -0300

    Correting the Conditional Structures folder

--- Conditional-Structures-UDEMY/ListaExerciciosCondicionais.cs
+++ Conditional-Structures-UDEMY/ListaExerciciosCondicionais.cs
@@ -10,9 +10,7 @@ namespace EstruturaCondicional
             Console.WriteLine("1) Exercício Resolvido 01");
             Console.WriteLine("2) Exercícico 02 - URI 1036");
             Console.WriteLine("3) Exercício Resolvido 03");
-            
             Console.WriteLine("5) SAIR");
-
         }
 
         public static void ExecutarExercicio()
@@ -26,7 +24,6 @@ namespace EstruturaCondicional
                 case 1: Exercicio01.Executar(); break;
                 case 2: Exercicio02.Executar(); break;
                 case 3: Exercicio03.Executar(); break;
-
 
                 case 5: 
                     Console.WriteLine("Saindo... Obrigado!");
