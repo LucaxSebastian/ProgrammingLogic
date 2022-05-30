dc3fbf3b965c17570524c32e0b3408df1224fd93
Lucas Sebastian <lucassebastian167@gmail.com>
2022-05-30 00:05:40 -0300

    Adding the Structure Do While

--- Repetitive-Structures-UDEMY/ListaExerciciosRepetitivas.cs
+++ Repetitive-Structures-UDEMY/ListaExerciciosRepetitivas.cs
@@ -9,9 +9,10 @@ namespace EstruturaRepetitiva
             Console.WriteLine("1) Exercício 01 - URI 1113 (Estrutura While)");  
             Console.WriteLine("2) Exercício 02 - Estrutura Repetitiva FOR");  
             Console.WriteLine("3) Exercício 03 - URI 1078 (Estrutura FOR)");  
-            Console.WriteLine("4) Exercício 04 - URI 1071 (Estrutura FOR)");   
+            Console.WriteLine("4) Exercício 04 - URI 1071 (Estrutura FOR)");
+            Console.WriteLine("5) Exercício 05 - Estrutura Repetitiva Do While");   
             
-            Console.WriteLine("5) SAIR");
+            Console.WriteLine("6) SAIR");
         }
 
         public static void ExecutarExercicio()
@@ -26,8 +27,9 @@ namespace EstruturaRepetitiva
                 case 2: EstruturaFOR.Executar(); break;
                 case 3: URI1078.Executar(); break;
                 case 4: URI1071.Executar(); break;
+                case 5: DoWhile.Executar(); break;
 
-                case 5: 
+                case 6: 
                     Console.WriteLine("Saindo... Obrigado!");
                     System.Environment.Exit(0);
                 break;
